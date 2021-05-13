3ac27b79ced8c27a9df13c87b0a320ddc0a10b78
denizyoldas <36186348+denizyoldas@users.noreply.github.com>
2021-05-13 19:35:27 +0300

    error fixed

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -18,9 +18,6 @@ namespace Business.DependencyResolvers.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
-            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
-
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
             builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
 
--- DataAccess/Concrete/EntityFramework/NortwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -14,10 +14,8 @@ namespace DataAccess.Concrete.EntityFramework
             optionsBuilder.UseSqlServer(@"server=(localdb)\mssqllocaldb;Database=Northwind; Trusted_Connection=true");
         }
 
-        public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Order> Orders { get; set; }
         public DbSet<OperationClaim> OperationClaims { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
